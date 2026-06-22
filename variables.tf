4b426a002fefa7513355e1e8992d529961c621c3
Rotha Dapravith <rothadapravith@Dapravith-MacBook-Pro.local>
2026-06-22 20:09:26 +0700

    fixed

--- variables.tf
+++ variables.tf
@@ -1,5 +1,11 @@
-variable "aws_region" {
-  description = "AWS region for EC2 deployment"
+variable "instance_type" {
+  description = "EC2 Instance Type"
   type        = string
-  default     = "us-east-1"
+  default = "t2.micro" # If terraform.tfvars is missing it will take t2.micro
+                       # otherwise it will take from terraform.tfvars
+}
+
+variable "aws_region" {
+  description = "AWS Region"
+  type        = string
 }
